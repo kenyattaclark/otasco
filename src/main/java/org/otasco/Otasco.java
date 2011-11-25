e3273cba8a9372e99e1b51fb562a77114892e02b
Kenyatta Clark <kenyatta.clark@gmail.com>
2011-11-25 08:17:28 -0600

    More refactorings.

--- src/main/java/org/otasco/Otasco.java
+++ src/main/java/org/otasco/Otasco.java
@@ -4,8 +4,45 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <ul>
+ * <li>Allows testing class under test without creating useless setters for dependencies.</li> 
+ * <li>Makes the test class more readable.</li>
+ * </ul>
+ * 
+ * <pre>
+ *   public class InvoiceManagerTest { 
+ *	     
+ *       &#064;Dependency     
+ *       private InvoiceCalculator invoiceCalculator;
+ *       
+ *       &#064;Dependency
+ *       private InvoiceDao invoiceDao;
+ *     
+ *     	 &#064;ClassUnderTest
+ *       private InvoiceManager manager;
+ *     
+ *       &#064;Before public void setup() {
+ *           invoiceCalculator = new InvoiceCalculator();
+ *           invoiceDao = new InvoiceDao();
+ *           manager = new InvoiceManager();
+ *           Otasco.init(this);
+ *       }
+ *   }
+ *   
+ * </pre>
+ * 
+ * <b><code>Otasco.init(this)</code></b> method has to called to "wire" the annotated instance of class under test to the annotated dependencies.
+ */
 public class Otasco {
 
+	/**
+	 * Wires objects annotated with &#064;ClassUnderTest for given testClass with objects annotated with &#064;Dependency.
+	 * <p>
+     * See examples in javadoc for {@link Otasco} class.
+	 * 
+	 * @param testClass
+	 */
 	public static void init(final Object testClass) {
 		if (testClass == null) {
 			throw new OtascoException("testClass cannot be null. For info on how to use @ClassUnderTest and @Dependency see examples in Javadoc for OtascoAnnotations class.");
@@ -55,11 +92,17 @@ public class Otasco {
 		}
 	}
 
-	public static void setField(final String fieldName, final Object value, final Object clazz) {
+	/**
+	 * Attempts to set the named field on a target object with the value passed in.
+	 * @param name
+	 * @param value
+	 * @param target
+	 */
+	public static void setField(final String name, final Object value, final Object target) {
 		try {
-			final Field field = clazz.getClass().getField(fieldName);
+			final Field field = target.getClass().getField(name);
 			makeAccessible(field);
-			field.set(clazz, value);
+			field.set(target, value);
 		} catch (Exception e) {
 			throw new OtascoException("Error setting field on class: " + e.getMessage(), e);
 		}
